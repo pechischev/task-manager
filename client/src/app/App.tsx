f9d7ee94bf877c3b62377443accfeb532bcf4341
Vladimir Pechischev <pechischev.vladimir@gmail.com>
2020-09-05 15:43:14 +0300

    feature: connect form and show columns

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
 
-import {Layout} from "../components/Layout/Layout";
-import {AddTask} from "../components/AddTask/AddTask";
+import {Layout} from '../components/Layout/Layout';
+import {AddTask} from '../components/AddTask/AddTask';
+import {Column} from '../components/Column';
+
+import {TaskForm} from './forms/TaskForm';
+
+const emptyForm = {isOpen: false, column: null}
 
 export const App: React.FunctionComponent = () => {
-  return <Layout>
-    {null}
-    <AddTask />
-  </Layout>
+  const [formData, setFormData] = useState(emptyForm)
+  const columns = useSelector((state: any) => state.columns)
+  const columnList = columns.map((column: any) => (
+    <Column
+      key={column.id}
+      title={column.title}
+      onAppend={() => {
+      }}
+      onChangeTitle={() => {
+      }}
+    >
+      <AddTask onClick={() => {
+        setFormData({isOpen: true, column: column.id});
+      }}/>
+    </Column>
+  ))
+
+  return (
+    <Layout>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        {columnList}
+        {formData.isOpen && (
+          <div>
+            <span onClick={() => setFormData(emptyForm)}>X</span>
+            <TaskForm onSubmit={(data) => console.log(data)}/>
+          </div>
+        )}
+      </div>
+    </Layout>
+  )
 }
